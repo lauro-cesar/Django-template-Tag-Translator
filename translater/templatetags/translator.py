1269c4110c40548a66623f1e81ff4f08724b7854
Lauro Cesar <olarva@gmail.com>
2011-07-23 09:52:52 -0700

    Edited translater/templatetags/translator.py via GitHub

--- translater/templatetags/translator.py
+++ translater/templatetags/translator.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from django import template
 from translater.models import myString
+from django.utils.encoding import smart_str,smart_unicode
 from django.conf import settings
 register = template.Library()
 import hashlib
 
 
+
+
 def TranslateMe(string,toLan):
+    try:
+        string= smart_str(string)
+    except:
+        string = smart_unicode(string)
     
     ident = hashlib.md5(string).hexdigest()
     translated,created = myString.objects.get_or_create(ident=ident,languageTo=toLan)
